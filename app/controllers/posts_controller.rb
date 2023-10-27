b62eb33451276ad95e7575ccf90839e4ffa3eac0
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-10-28 01:47:29 +0900

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    if post.save
+      redirect_to post_path
+    else
+      @post = post
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def post_params
