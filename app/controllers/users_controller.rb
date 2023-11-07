2048fa560ce1ef4b9b79f271856ce2092e980a03
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-11-07 22:50:08 +0900

    退会機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # @post = current_user.run_spot
   end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to root_path
+  end
 end
