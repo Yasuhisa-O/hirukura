f1022473874a5f619a0a21f6c7e5223230691e8d
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-11-09 22:30:51 +0900

    userのshowに、投稿記事表示を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # @post = current_user.run_spot
+    @posts = @user.posts.includes(:user)
   end
 
   def destroy
