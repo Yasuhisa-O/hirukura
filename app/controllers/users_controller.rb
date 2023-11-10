02fcb51556741b94b5e740d496ecd52140f5dc63
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-11-10 22:26:53 +0900

    登録ユーザーの名前を変更可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
     @posts = @user.posts.includes(:user)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    if user.save
+      redirect_to post_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
   def destroy
     user = User.find(params[:id])
     user.destroy
