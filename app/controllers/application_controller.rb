428e26e4cd96a865294868d2fbd60ababf5827fb
Yasuhisa-O <koriyama.o4m@gmail.com>
2023-11-12 15:47:22 +0900

    deviseのコントローラを導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :prefecture_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :prefecture_id])
   end
 end
